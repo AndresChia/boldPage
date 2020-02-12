1e046688629330917109dad3ab26c7f65a67dbf5
Andres_Chia <andres_chia11@hotmail.com>
2020-02-12 17:11:10 -0500

    COMMIT: cambios finales

--- src/Components/LeaderBoards/Ranking/Ranking.js
+++ src/Components/LeaderBoards/Ranking/Ranking.js
@@ -1,6 +1,28 @@
 import React from 'react'
 import styles from './style.module.scss';
 import userIcon from './../../../Assets/Images/userIcon.png'
+import priceIcon from './../../../Assets/Images/priceIcon.png'
+
+
+
+const styleFirstRanking = {
+    textAlign: 'center',
+    top: '-5.5vw',
+    position: 'relative',
+    color: '#ffffff',
+    fontFamily: 'Montserrat-ExtraLight',
+};
+
+
+const styleSYTRanking = {
+    textAlign: 'center',
+    top: '12vw',
+    position: 'relative',
+    color: '#ffffff',
+    fontFamily: 'Montserrat-ExtraLight',
+};
+
+
 
 const retornarDatos = (arreglo) => {
     return arreglo.map((element) => (
@@ -23,16 +45,46 @@ export const Ranking = ({ arreglo }) => {
     return (
         <div className={styles.contenedorRanking}>
 
+
+            <div className={styles.tuPosicionContenedor}>
+                <h5>Tu posición <strong>#150 </strong></h5>
+            </div>
+
+
             <div className={styles.contenedorTopSuperior}>
-                <div className={styles.segundoPuesto}>
-                    <img src={userIcon} alt="UserIcon"></img>
+
+
+                <div id="elemento-2" className={styles.contenedorCirculos}>
+                    <div className={styles.segundoPuestoCirculo}>
+                        <img src={userIcon} alt="UserIcon"></img>
+                        <div className={styles.puesto}><h5>2</h5></div>
+                    </div>
+                    <h5 style={styleSYTRanking}>jo**********@g********</h5>
                 </div>
-                <div className={styles.primerPuesto}>
-                    <img src={userIcon} alt="UserIcon"></img>
+
+
+                <div id="elemento-1" className={styles.contenedorCirculosCentro}>
+                    <div className={styles.primerPuestoCirculo}>
+                        <img src={userIcon} alt="UserIcon"></img>
+                        <img className={styles.price} src={priceIcon} alt="priceIcon"></img>
+                        <div className={styles.puesto}><h5>1</h5></div>
+                    </div>
+                    <h5 style={styleFirstRanking}>pi**********@e**************</h5>
                 </div>
-                <div className={styles.tercerPuesto}>
-                    <img src={userIcon} alt="UserIcon"></img>
+
+
+
+                <div id="elemento-3" className={`${styles.contenedorTercerPuesto} ${styles.contenedorCirculos}`}>
+                    <div className={styles.tercerPuestoCirculo}>
+                        <img src={userIcon} alt="UserIcon"></img>
+                        <div className={styles.puesto}><h5>3</h5></div>
+                    </div>
+                    <h5 style={styleSYTRanking}>jh****@g********</h5>
                 </div>
+
+
+
+
             </div>
 
             <div className={styles.contenedorTopInferior}>
